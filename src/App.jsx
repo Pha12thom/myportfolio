a2555c9a756696620e7281097f39caaeebc4f5bb
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-01-22 20:46:40 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
+  name: "Milugo Geofrey Ouma",
+  title: "Web Designer , software engineer, Ai engineer",
+  email: "milugogeofrey0@gmail.com",
+  gitHub: "https://github.com/pha12thom",
+  instagram: "https://www.instagram.com/milugo_g",
+  linkedIn: "https://www.linkedin.com/in/geofrey-milugo-msu27345/",
+  medium: "https://medium.com/@geofreymilugo",
+  twitter: "https://x.com/milugo_g",
   youTube: "Code",
 };
 
